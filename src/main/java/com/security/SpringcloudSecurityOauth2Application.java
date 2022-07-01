c01699a718fa97bc5598a3be5e0a95da129e9e03
changhuan_wan <changhuang_wan@windmagics.com>
2022-07-01 10:52:11 +0800

    init commit

---
+++ src/main/java/com/security/SpringcloudSecurityOauth2Application.java
@@ -0,0 +1,13 @@
+package com.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudSecurityOauth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudSecurityOauth2Application.class, args);
+	}
+
+}
